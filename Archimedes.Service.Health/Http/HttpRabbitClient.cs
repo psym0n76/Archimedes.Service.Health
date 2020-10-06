695cbf5b1762068e2272026035b227fbce4fdd8d
Simon Burton <burt76@outlook.com>
2020-10-06 21:24:35 +0100

    fixed rabbit health check

--- Archimedes.Service.Health/Http/HttpRabbitClient.cs
+++ Archimedes.Service.Health/Http/HttpRabbitClient.cs
@@ -10,14 +10,15 @@ namespace Archimedes.Service.Ui.Http
     public class HttpRabbitClient : IHttpRabbitClient
     {
         private readonly HttpClient _client;
+        private readonly Config _config;
 
         //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-3.1
 
         public HttpRabbitClient(IOptions<Config> config, HttpClient httpClient)
         {
             httpClient.BaseAddress = new Uri($"http://{config.Value.RabbitHost}:1{config.Value.RabbitPort}");
-            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
             _client = httpClient;
+            _config = config.Value;
         }
 
         public async Task<HealthMonitorDto> GetHealth()
@@ -28,7 +29,7 @@ namespace Archimedes.Service.Ui.Http
             {
                 Url = response.RequestMessage.RequestUri.ToString(),
                 LastUpdated = DateTime.Now,
-                AppName = $"Rabbit",
+                AppName = "Archimedes.Rabbit",
                 Version = "1.0"
             };
 
