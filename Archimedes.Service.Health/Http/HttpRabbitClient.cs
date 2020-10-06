537b5962e88ea0256678d7745ecef814ef974bb3
Simon Burton <burt76@outlook.com>
2020-10-06 22:18:05 +0100

    health

--- Archimedes.Service.Health/Http/HttpRabbitClient.cs
+++ Archimedes.Service.Health/Http/HttpRabbitClient.cs
@@ -10,6 +10,7 @@ namespace Archimedes.Service.Ui.Http
     public class HttpRabbitClient : IHttpRabbitClient
     {
         private readonly HttpClient _client;
+        private readonly Config _config;
 
         //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-3.1
 
@@ -17,6 +18,7 @@ namespace Archimedes.Service.Ui.Http
         {
             httpClient.BaseAddress = new Uri($"http://{config.Value.RabbitHost}:1{config.Value.RabbitPort}");
             _client = httpClient;
+            _config = config.Value;
         }
 
         public async Task<HealthMonitorDto> GetHealth()
@@ -27,7 +29,7 @@ namespace Archimedes.Service.Ui.Http
                 LastUpdated = DateTime.Now,
                 AppName = "Archimedes.Rabbit",
                 Version = "1.0.0",
-                Url = "http://localhost:15673"
+                Url = $"http://{_config.RabbitHost}:1{_config.RabbitPort}"
             };
 
             try
