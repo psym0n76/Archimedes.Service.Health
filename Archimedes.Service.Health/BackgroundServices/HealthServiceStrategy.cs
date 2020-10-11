8afdbf585ad6958420b3bfe76b690783e2ff4fd6
Simon Burton <burt76@outlook.com>
2020-10-11 12:31:22 +0100

    added stratgy health

---
+++ Archimedes.Service.Health/BackgroundServices/HealthServiceStrategy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Archimedes.Service.Ui.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Archimedes.Service.Health
+{
+    public class HealthServiceStrategy : BackgroundService
+    {
+
+        private readonly IHttpBrokerClient _httpClient;
+        private readonly IHealthDataStore _healthDataStore;
+        private readonly ILogger<HealthServiceStrategy> _logger;
+
+        public HealthServiceStrategy(IHttpBrokerClient httpClient, IHealthDataStore healthDataStore, ILogger<HealthServiceStrategy> logger)
+        {
+            _httpClient = httpClient;
+            _healthDataStore = healthDataStore;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                try
+                {
+                    stoppingToken.ThrowIfCancellationRequested();
+                    _logger.LogInformation($"Running HealthServiceStrategy");
+                    await UpdateStrategyHealth();
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError($"Error found in HealthServiceStrategy: {e.Message} {e.StackTrace}");
+                }
+
+                Thread.Sleep(15000);
+            }
+        }
+
+        private async Task UpdateStrategyHealth()
+        {
+            var response = await _httpClient.GetHealth();
+            _healthDataStore.Update(response);
+        }
+    }
+}
---
+++ Archimedes.Service.Health/Http/HttpStrategyClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Archimedes.Library.Domain;
+using Archimedes.Library.Message.Dto;
+using Microsoft.Extensions.Options;
+
+namespace Archimedes.Service.Ui.Http
+{
+    public class HttpStrategyClient : IHttpStrategyClient
+    {
+        private readonly HttpClient _client;
+        private readonly IHealthResponse _healthResponse;
+
+        //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-3.1
+
+        public HttpStrategyClient(IOptions<Config> config, HttpClient httpClient, IHealthResponse healthResponse)
+        {
+            httpClient.BaseAddress = new Uri($"{config.Value.StrategyUrl}");
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            _client = httpClient;
+            _healthResponse = healthResponse;
+        }
+
+
+        public async Task<HealthMonitorDto> GetStrategyHealth()
+        {
+            var response = await _client.GetAsync("health");
+
+            var healthResponse =  await _healthResponse.Build(response);
+            return healthResponse;
+        }
+    }
+}
